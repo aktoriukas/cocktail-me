fce2d58eb5aed71104857efecb4d98ef0384f3fa
aktoriukas <gediminas.strumila@gmail.com>
2021-02-18 12:21:18 +0000

    design

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './css/index.css'
 
-import Navigation from './Components/Navigation';
-import About from './Pages/About';
+import Navigation from './Components/Navigation'
+import About from './Pages/About'
 import Cocktails from './Pages/Cocktails'
 import Home from './Pages/Home'
+import Background from './Components/Background'
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
@@ -17,6 +18,7 @@ function App() {
           <Route path='/about' component={About} />
           <Route path='/cocktails' exact component={Cocktails} />
         </Switch>
+        <Background />
       </div>
     </Router>
   );
---
+++ src/Components/Background.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import img from '../img/drink.jpg'
+
+export default function Background() {
+    return (
+        <section id='background'>
+            <img src={img} alt='background' />
+        </section>
+    )
+}
